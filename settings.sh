faf8f9cd07385ce9b34df32e3dc53541e3ac20a9
Iztok Jeras <iztok.jeras@gmail.com>
2017-09-13 11:10:37 +0200

    AXI memory mapped example now compiles

--- settings.sh
+++ settings.sh
@@ -2,7 +2,7 @@
 # setup Xilinx Vivado FPGA tools
 ################################################################################
 
-. /mnt/cb9307c5-4ab6-49fc-9f92-3018868ee86e/opt/Xilinx/Vivado/2017.2/settings64.sh
+. /opt/Xilinx/Vivado/2017.2/settings64.sh
 
 ################################################################################
 # setup cross compiler toolchain
