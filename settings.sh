9230017a57fe18b559f370acb351d6ce4bf7fc56
Iztok Jeras <iztok.jeras@gmail.com>
2020-06-04 00:51:11 +0200

    unfinished update

--- settings.sh
+++ settings.sh
@@ -2,10 +2,4 @@
 # setup Xilinx Vivado FPGA tools
 ################################################################################
 
-. /opt/Xilinx/Vivado/2017.3/settings64.sh
-
-################################################################################
-# setup cross compiler toolchain
-################################################################################
-
-export CROSS_COMPILE=arm-linux-gnueabihf-
+. /opt/Xilinx/Vivado/2018.1/settings64.sh
